8b4e4a376f36098c228a4196c90f47e67932db5a
木偶 <7885290+liao_shengrong@user.noreply.gitee.com>
2023-10-22 01:44:29 +0800

    remove

--- next.config.js
+++ next.config.js
@@ -3,13 +3,10 @@ const path = require('path')
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-  }.includePaths,
+  },
   reactStrictMode: false,
   // Enable source map if needed
   productionBrowserSourceMaps: true,
-  experimental: {
-    serverAction: true
-  }
   // i18n: {
   //   defaultLocale: 'en',
   //   locales: ['en', 'zh'],
