bb824a8eb80dd577a6f42c5516759f4e5062a155
ShengRong Liao <shengrong.liao@prudential.com.sg>
2023-10-27 18:04:16 +0800

    api test 8

--- next.config.js
+++ next.config.js
@@ -1,15 +1,36 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
+const path = require("path");
 const nextConfig = {
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
-  output: 'export',
+  output: "export",
   reactStrictMode: false,
   // Enable source map if needed
   productionBrowserSourceMaps: true,
   images: {
-    domains: ['cdn.waifu.im', 'power-api.cretinzp.com', 'images.wallpaperscraft.com'],
+    domains: [
+      "cdn.waifu.im",
+      "power-api.cretinzp.com",
+      "images.wallpaperscraft.com",
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Referrer",
+            value: "origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
   },
   // async rewrites() {
   //   return [
@@ -25,7 +46,6 @@ const nextConfig = {
   //   locales: ['en', 'zh'],
   //   localePath: path.resolve('./public/locales'),
   // },
-}
+};
 
-
-module.exports = nextConfig
+module.exports = nextConfig;
