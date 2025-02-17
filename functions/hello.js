6459491a4bb2002990b1d69ca42deec3c541f6b9
shengrong <shengrong.liao@epro.com.cn>
2025-02-17 10:49:14 +0800

    docs(hello): 添加自动化约定路由功能
    - 新增自动化约定路由插件，用于自动生成路由配置
    - 创建路由配置文件、页面导出文件和路由类型定义文件
    - 实现路由自动生成、页面组件懒加载和路由提供者配置
    - 添加相关代码示例和说明文档

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,10 @@
+const txt = require("./md-text/自动化约定路由.txt");
 exports.handler = async function (event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello, World! 我自定义的无服务器函数" }),
+    body: JSON.stringify({
+      message: "Hello, World! 我自定义的无服务器函数",
+      txt,
+    }),
   };
 };
