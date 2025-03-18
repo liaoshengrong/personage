ce54a91c7e61f620948b01a995af778b03c5e21e
shengrong <shengrong.liao@epro.com.cn>
2025-03-18 16:20:41 +0800

    feat(wallpaper): 解析请求体并增强返回数据
    在handler函数中解析event.body，并在返回数据中添加标题字段和请求体信息，以增强API的功能性和响应内容。

--- functions/wallpaper.js
+++ functions/wallpaper.js
@@ -1,6 +1,8 @@
 // https://api.hn/acg.php
 import { CORS_HEADERS } from "./utils/common";
 exports.handler = async function (event, context) {
+  const requestBody = JSON.parse(event.body);
+
   if (event.httpMethod === "OPTIONS") {
     return {
       statusCode: 200,
@@ -11,9 +13,16 @@ exports.handler = async function (event, context) {
 
   const res = await fetch(`https://api.hn/acg.php?zd=pc&return=jsonpro`);
   const data = await res.json();
+
   return {
     statusCode: 200,
     headers: CORS_HEADERS,
-    body: JSON.stringify({ data }),
+    body: JSON.stringify({
+      data: data.map((v, i) => ({
+        ...v,
+        title: "壁纸" + (i + 1),
+      })),
+      requestBody,
+    }),
   };
 };
