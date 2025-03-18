5fa055f09eed1c44fe70be387d01529b5857b5d3
shengrong <shengrong.liao@epro.com.cn>
2025-03-18 16:52:36 +0800

    refactor(wallpaper.js): 优化分页逻辑并移除冗余字段
    将 `pageSize` 的转换方式从 `parseInt` 改为 `Number`，确保类型一致。调整 `paginatedData` 的分页逻辑，直接使用 `pageSize` 进行切片，简化代码。移除返回数据中冗余的 `event` 字段。

--- functions/wallpaper.js
+++ functions/wallpaper.js
@@ -13,16 +13,16 @@ exports.handler = async function (event, context) {
   const data = await res.json();
 
   const page = Number(event.queryStringParameters?.page) || 1;
-  const pageSize = parseInt(event.queryStringParameters?.pageSize) || 10;
+  const pageSize = Number(event.queryStringParameters?.pageSize) || 10;
 
   const startIndex = (page - 1) * pageSize;
   const endIndex = startIndex + pageSize;
 
   const _data = data?.map((v, i) => ({
     ...v,
-    title: "壁纸" + (i + 1),
+    title: "壁纸" + (i + 1) * page,
   }));
-  const paginatedData = _data?.slice(startIndex, endIndex);
+  const paginatedData = _data?.slice(0, pageSize);
 
   return {
     statusCode: 200,
@@ -32,7 +32,6 @@ exports.handler = async function (event, context) {
       total: _data?.length || 0,
       page,
       pageSize,
-      event,
     }),
   };
 };
