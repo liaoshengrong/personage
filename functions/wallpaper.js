3dbe348f80134d3baa41632040ed765b05ec97ec
shengrong <shengrong.liao@epro.com.cn>
2025-03-18 16:42:22 +0800

    refactor(wallpaper.js): 移除分页逻辑以简化数据返回
    移除与分页相关的代码，直接返回完整数据集，简化了数据处理的逻辑并减少了不必要的计算。

--- functions/wallpaper.js
+++ functions/wallpaper.js
@@ -12,26 +12,26 @@ exports.handler = async function (event, context) {
   const res = await fetch(`https://api.hn/acg.php?zd=pc&return=jsonpro`);
   const data = await res.json();
 
-  const page = parseInt(event.queryStringParameters?.page) || 1;
-  const pageSize = parseInt(event.queryStringParameters?.pageSize) || 10;
+  // const page = parseInt(event.queryStringParameters?.page) || 1;
+  // const pageSize = parseInt(event.queryStringParameters?.pageSize) || 10;
 
-  const startIndex = (page - 1) * pageSize;
-  const endIndex = startIndex + pageSize;
+  // const startIndex = (page - 1) * pageSize;
+  // const endIndex = startIndex + pageSize;
 
   const _data = data?.map((v, i) => ({
     ...v,
-    title: "壁纸" + (i + 1) * page,
+    title: "壁纸" + (i + 1),
   }));
-  const paginatedData = _data?.slice(startIndex, endIndex);
+  // const paginatedData = _data?.slice(startIndex, endIndex);
 
   return {
     statusCode: 200,
     headers: CORS_HEADERS,
     body: JSON.stringify({
-      data: paginatedData,
+      data: _data,
       total: _data?.length || 0,
-      page,
-      pageSize,
+      // page,
+      // pageSize,
       event,
     }),
   };
