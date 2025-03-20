8d2313b4d276c6dae5795f29ea567980475b1c3d
shengrong <shengrong.liao@epro.com.cn>
2025-03-20 14:46:07 +0800

    feat(wallpaper): 添加静态壁纸数据以替代 API 调用

--- functions/wallpaper.js
+++ functions/wallpaper.js
@@ -1,4 +1,66 @@
 // https://api.hn/acg.php
+const arr = [
+  {
+    code: "200",
+    imgurl: "https://api.hn/acg/API.HN_ACG_5543.png?t=1742453042",
+    width: 1052,
+    height: 760,
+  },
+  {
+    code: "200",
+    imgurl: "https://api.hn/acg/API.HN_ACG_10797.jpg?t=1742453042",
+    width: 5600,
+    height: 3150,
+  },
+  {
+    code: "200",
+    imgurl: "https://api.hn/acg/API.HN_ACG_2635.jpg?t=1742453042",
+    width: 2000,
+    height: 2000,
+  },
+  {
+    code: "200",
+    imgurl: "https://api.hn/acg/API.HN_ACG_1161.png?t=1742453042",
+    width: 2260,
+    height: 3213,
+  },
+  {
+    code: "200",
+    imgurl: "https://api.hn/acg/API.HN_ACG_853.png?t=1742453042",
+    width: 1000,
+    height: 1000,
+  },
+  {
+    code: "200",
+    imgurl: "https://api.hn/acg/API.HN_ACG_817.jpg?t=1742453042",
+    width: 3200,
+    height: 1800,
+  },
+  {
+    code: "200",
+    imgurl: "https://api.hn/acg/API.HN_ACG_2091.png?t=1742453042",
+    width: 4096,
+    height: 2560,
+  },
+  {
+    code: "200",
+    imgurl: "https://api.hn/acg/API_ACG_0000000000000676.webp?t=1742453042",
+    width: 2501,
+    height: 1349,
+  },
+  {
+    code: "200",
+    imgurl: "https://api.hn/acg/API.HN_ACG_3045.png?t=1742453042",
+    width: 1400,
+    height: 1050,
+  },
+  {
+    code: "200",
+    imgurl: "https://api.hn/acg/API.HN_ACG_10656.jpg?t=1742453042",
+    width: 1400,
+    height: 1100,
+  },
+];
 import { CORS_HEADERS } from "./utils/common";
 exports.handler = async function (event, context) {
   if (event.httpMethod === "OPTIONS") {
@@ -10,7 +72,8 @@ exports.handler = async function (event, context) {
   }
 
   const res = await fetch(`https://api.hn/acg.php?zd=pc&return=jsonpro`);
-  const data = await res.json();
+  // const data = await res.json();
+  const data = arr;
 
   const page = Number(event.queryStringParameters?.page) || 1;
   const pageSize = Number(event.queryStringParameters?.pageSize) || 10;
