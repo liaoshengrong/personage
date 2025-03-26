662216c2f33d0b967202ea42dc56c27ecb9ea880
shengrong <shengrong.liao@epro.com.cn>
2025-03-26 17:22:38 +0800

    refactor: 更新AI助手的自我介绍并恢复demo页面的功能
    更新了AI助手的自我介绍，增加了毕业院校和工作经验等详细信息。同时，恢复了demo页面的组件导入和功能逻辑，移除了临时的占位代码。

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,41 +1,33 @@
-// import React, { use } from "react";
-// import Navbar from "../_components/Navbar";
-// import PageContainer from "../_components/PageContainer";
-// import Drag from "./_components/Drag";
-// import Tabs from "./_components/Tabs";
-// import ShortVideo from "./_components/ShortVideo";
-// import { getVideoData, getWallpaper } from "./api";
-// import Wallpaper from "./_components/Wallpaper";
+import React, { use } from "react";
+import Navbar from "../_components/Navbar";
+import PageContainer from "../_components/PageContainer";
+import Drag from "./_components/Drag";
+import Tabs from "./_components/Tabs";
+import ShortVideo from "./_components/ShortVideo";
+import { getVideoData, getWallpaper } from "./api";
+import Wallpaper from "./_components/Wallpaper";
 
-// const Index = ({
-//   searchParams,
-// }: {
-//   searchParams: Promise<{ tab?: string }>;
-// }) => {
-//   const video = use(getVideoData());
-//   const wallpapers = use(getWallpaper());
-//   const { tab } = searchParams ? use(searchParams) : {};
+const Index = ({
+  searchParams,
+}: {
+  searchParams: Promise<{ tab?: string }>;
+}) => {
+  const video = use(getVideoData());
+  const wallpapers = use(getWallpaper());
+  const { tab } = searchParams ? use(searchParams) : {};
 
-//   return (
-//     <PageContainer>
-//       <Navbar />
-//       <Tabs titles={tabs} defalultTab={tab}>
-//         <ShortVideo data={[video]} />
-//         <Drag />
-//         <Wallpaper data={wallpapers} />
-//       </Tabs>
-//     </PageContainer>
-//   );
-// };
-
-// export default Index;
-
-// const tabs = ["刷短视频", "文件解析", "精选壁纸"];
-
-import React from "react";
-
-const page = () => {
-  return <div>page</div>;
+  return (
+    <PageContainer>
+      <Navbar />
+      <Tabs titles={tabs} defalultTab={tab}>
+        <ShortVideo data={[video]} />
+        <Drag />
+        <Wallpaper data={wallpapers} />
+      </Tabs>
+    </PageContainer>
+  );
 };
 
-export default page;
+export default Index;
+
+const tabs = ["刷短视频", "文件解析", "精选壁纸"];
