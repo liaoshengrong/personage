ef326b1996d4a81137864843b660faa1c6cc522c
shengrong <shengrong.liao@epro.com.cn>
2025-05-26 14:15:19 +0800

    refactor(sitemap): 更新网站 URL 为 Netlify 新域名
    - 将 next-sitemap 配置文件中的 siteUrl 从 "https://shengrong.netlify.com" 修改为 "https://shengrong.netlify.app"
    - 更新 robots.txt 和 sitemap.xml 文件中的 URL 为新的 Netlify 域名
    - 调整 sitemap-0.xml 中的 URL 为新的域名，并更新最后修改时间

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://shengrong.netlify.com",
+  siteUrl: "https://shengrong.netlify.app",
   generateRobotsTxt: true, // 自动生成 robots.txt
   sitemapSize: 5000,
   //   exclude: ["/private-page", "/admin/*"],
