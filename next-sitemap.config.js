4accc380118a9dadb89dc5a11fd30aa15cd82fce
shengrong <shengrong.liao@epro.com.cn>
2025-05-26 14:12:21 +0800

    feat(sitemap): 生成 sitemap 和 robots.txt 文件
    - 更新 next-sitemap 配置，设置 siteUrl 和其他参数
    - 生成 robots.txt 文件，允许所有机器人访问
    - 创建 sitemap-0.xml 和 sitemap.xml 文件，包含网站 URL

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://your-blog-domain.com",
+  siteUrl: "https://shengrong.netlify.com",
   generateRobotsTxt: true, // 自动生成 robots.txt
   sitemapSize: 5000,
-  exclude: ["/private-page", "/admin/*"],
-  // changefreq: 'daily',
-  // priority: 0.7,
+  //   exclude: ["/private-page", "/admin/*"],
+  //   changefreq: "daily",
+  autoLastmod: true,
+  priority: 1,
 };
